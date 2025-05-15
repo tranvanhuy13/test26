30a9918d754631ade45990a919dd38b048ef068c
anhtu <anhtu@tuan13042005>
2025-05-15 19:40:18 +0700

    tru

--- src/test26/Test26.java
+++ src/test26/Test26.java
@@ -5,7 +5,9 @@ public class Test26 {
     public static int cong (int a, int b){
         return a + b ;
     }
-
+    public static int tru(int a, int b){
+        return a - b;
+    }
     
     public static void main(String[] args) {
         System.out.println("test26");
